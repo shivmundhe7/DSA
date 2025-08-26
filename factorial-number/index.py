aeb831361901eb083f6d4eff80846e4a662ffff4
shivmundhe7 <shivmundhe16@gmail.com>
2025-08-27 00:01:18 +0530

    DSA

---
+++ factorial-number/index.py
@@ -0,0 +1,11 @@
+def factorial(n):
+
+    res = 1
+
+    for i in range(2, n + 1):
+        res *= i
+    return res
+
+
+num = 5
+print("Factorial of", num, "is", factorial(num))
--- overlaping-traingle/index.py
+++ overlaping-traingle/index.py
@@ -25,3 +25,4 @@ if __name__ == "__main__":
         print("Rectangles Overlap")
     else:
         print("Rectangles Don't Overlap")
+        
