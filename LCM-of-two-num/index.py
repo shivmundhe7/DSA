2e2ac2edea41476b7b8ef50d104a10e5c31080c4
shivmundhe7 <shivmundhe16@gmail.com>
2025-08-29 00:03:26 +0530

    DSA

--- LCM-of-two-num/index.py
+++ LCM-of-two-num/index.py
@@ -1,15 +1,15 @@
 def lcm(a, b):
     
-    # Larger value
+
     g = max(a, b) 
-    
-    # Smaller value
+
     s = min(a, b)  
 
     for i in range(g, a * b + 1, g):
         if i % s == 0:
             return i
     return a * b 
+
 
 if __name__ == '__main__':
     a = 10
