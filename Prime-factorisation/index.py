008c43d926340fff64ddb20eafa81400c792807d
shivmundhe7 <shivmundhe16@gmail.com>
2025-09-15 13:19:57 +0530

    dsa

---
+++ Prime-factorisation/index.py
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class GfG {
+
+    public static ArrayList<Integer> primeFac(int n) {
+        ArrayList<Integer> res = new ArrayList<>();
+
+        // Check for factor 2
+        if (n % 2 == 0) {
+            res.add(2);
+            while (n % 2 == 0) {
+                n /= 2;
+            }
+        }
+
+        for (int i = 3; i <= Math.sqrt(n); i += 2) {
+            if (n % i == 0) {
+                res.add(i);
+                while (n % i == 0) {
+                    n /= i;
+                }
+            }
+        }
+
+        // If remaining n is a prime number > 2
+        if (n > 2) {
+            res.add(n);
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int n = 100;
+        ArrayList<Integer> result = primeFac(n);
+
+        for (int factor : result) {
+            System.out.print(factor + " ");
+        }
+    }
+}
