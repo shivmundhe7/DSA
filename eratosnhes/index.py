2e11b5d3d8d977fff006d6db5f4f249eceedd85e
shivmundhe7 <shivmundhe16@gmail.com>
2025-10-04 22:44:10 +0530

    dsa

--- eratosnhes/index.py
+++ eratosnhes/index.py
@@ -5,16 +5,13 @@ def sieve(n):
 
     while p * p <= n:
         if prime[p]:
-            
             for i in range(p * p, n + 1, p):
                 prime[i] = False
         p += 1
-
     res = []
     for p in range(2, n + 1):
         if prime[p]:
             res.append(p)
-    
     return res
 
 if __name__ == "__main__":
