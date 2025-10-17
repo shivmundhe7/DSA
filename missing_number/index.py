130661bd9ac7cce1cab484152a7008aa32080b68
shivmundhe7 <shivmundhe16@gmail.com>
2025-10-17 10:54:46 +0530

    dsa

--- missing_number/index.py
+++ missing_number/index.py
@@ -1,5 +1,3 @@
-# Python3 program to find the smallest
-# elements missing in a sorted array.
 
 def findFirstMissing(array, start, end):
 
@@ -11,7 +9,6 @@ def findFirstMissing(array, start, end):
 
     mid = int((start + end) / 2)
 
-    # Left half has all elements
     # from 0 to mid
     if (array[mid] == mid):
         return findFirstMissing(array,
@@ -21,10 +18,8 @@ def findFirstMissing(array, start, end):
                           start, mid)
 
 
-# driver program to test above function
 arr = [0, 1, 2, 3, 4, 5, 6, 7, 10]
 n = len(arr)
 print("Smallest missing element is",
       findFirstMissing(arr, 0, n-1))
 
-# This code is contributed by Smitha Dinesh Semwal
