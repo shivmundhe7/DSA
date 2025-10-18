4888dbd8f0e48f6c4d202287cb4a4e5c384da83a
shivmundhe7 <shivmundhe16@gmail.com>
2025-10-18 17:14:05 +0530

    dsa

---
+++ sub-array-given-sum/index.py
@@ -0,0 +1,23 @@
+def subarraySum(arr, target):
+    res = []
+    n = len(arr)
+
+    for s in range(n):
+        curr = 0
+
+        for e in range(s, n):
+            curr += arr[e]
+            if curr == target:
+                res.append(s + 1)
+                res.append(e + 1)
+                return res
+
+    return [-1]
+  	
+if __name__ == "__main__":
+    arr = [15, 2, 4, 8, 9, 5, 10, 23]
+    target = 23
+    res = subarraySum(arr, target)
+
+    for ele in res:
+        print(ele, end=" ")
