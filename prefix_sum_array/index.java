d8d72c9cc2ad4a5a410b79b26b89dde8b593d105
shivmundhe7 <shivmundhe16@gmail.com>
2025-10-22 11:23:04 +0530

    dsa

---
+++ prefix_sum_array/index.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class index {
+    public static ArrayList<Integer> prefSum(int[] arr) {
+        int n = arr.length;
+        ArrayList<Integer> prefixSum = new ArrayList<>();
+
+        prefixSum.add(arr[0]);
+        for (int i = 1; i < n; i++)
+            prefixSum.add(prefixSum.get(i - 1) + arr[i]);
+
+        return prefixSum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 10, 20, 10, 5, 15 };
+        ArrayList<Integer> prefixSum = prefSum(arr);
+        for (int i : prefixSum) {
+            System.out.print(i + " ");
+        }
+    }
+}
