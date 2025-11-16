2ced1ee8e14a046c6ee05cee8b4bea5f7beb5009
shivmundhe7 <shivmundhe16@gmail.com>
2025-11-16 15:40:09 +0530

    dsa

--- print-alternates/index.py
+++ print-alternates/index.py
@@ -1,14 +1,8 @@
-# Iterate Python Program to print alternate elements
-# of the array
-
 def getAlternates(arr):
     res = []
-    
-    # Iterate over all alternate elements
-    for i in range(0, len(arr), 2):
+        for i in range(0, len(arr), 2):
         res.append(arr[i])
     return res
-
 if __name__ == "__main__":
     arr = [10, 20, 30, 40, 50]
     res = getAlternates(arr)
